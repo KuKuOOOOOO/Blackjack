3caeb95b2e26318de07f9cda853b7a68fe3ecccd
Samuel Hopp <sjhopp@gmail.com>
2013-02-03 14:19:18 -0800

    Updated some of the objects and reorganized.  Will add control binding next

--- BlackJack/ViewModels/BlackjackTableViewModel.cs
+++ BlackJack/ViewModels/BlackjackTableViewModel.cs
@@ -5,8 +5,47 @@
   using System.Linq;
   using System.Text;
   using WPF.ViewModels;
+  using WPF.AttachedCommandBehavior;
 
   public class BlackjackTableViewModel : ViewModelBase
   {
+    private List<CardControlViewModel> cards;
+    public List<CardControlViewModel> Cards
+    {
+      get
+      {
+        return this.cards;
+      }
+      set
+      {
+        if (value == this.cards)
+        {
+          return;
+        }
+
+        this.cards = value;
+        this.OnPropertyChanged("Cards");
+      }
+    }
+
+    public SimpleCommand HitMeCommand;
+
+    public BlackjackTableViewModel()
+    {
+      HitMeCommand = new SimpleCommand();
+
+      this.CommandSink.RegisterCommand(HitMeCommand, CanHit, HitMe);
+    }
+
+    public bool CanHit(object o)
+    {
+      return true;
+    }
+
+    public void HitMe(object o)
+    {
+      this.Cards.Add(new CardControlViewModel(1));
+      this.OnPropertyChanged("Cards");
+    }
   }
 }
